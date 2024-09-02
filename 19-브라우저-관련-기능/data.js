915f686729244d232959342dc7827c759dfb6cc0
Gilju Song <rlfwn@gmail.com>
2024-09-02 17:48:22 +0900

    .

--- 19-브라우저-관련-기능/data.js
+++ 19-브라우저-관련-기능/data.js
@@ -4,19 +4,19 @@ const webtoon = {
             title: "참교육",
             author: "채용택/한가람",
             point: 9.86,
-            thumbnail: "mon1.jpg"
+            thumbnail:"./mon1.jpg"
         },
         {
             title: "뷰티풀 군바리",
             author: "설이/윤성원",
             point: 9.81,
-            thumbnail: "mon2.jpg"
+            thumbnail: "./mon2.jpg"
         },
         {
             title: "퀘스트지상주의",
             author: "박태준 만화회사",
             point: 9.79,
-            thumbnail: "mon3.jpg"
+            thumbnail: "./mon3.jpg"
         }
     ],
 
@@ -25,19 +25,19 @@ const webtoon = {
             title: "김부장",
             author: "박태준 만화회사/정종택",
             point: 9.19,
-            thumbnail: "tue1.jpg"
+            thumbnail: "./tue1.jpg"
         },
         {
             title: "여신강림",
             author: "야옹이",
             point: 9.38,
-            thumbnail: "tue2.jpg"
+            thumbnail: "./tue2.jpg"
         },
         {
             title: "대학원 탈출일지",
             author: "요다",
             point: 9.98,
-            thumbnail: "tue3.jpg"
+            thumbnail: "./tue3.jpg"
         }
     ],
 
@@ -46,19 +46,103 @@ const webtoon = {
             title: "내 남편과 결혼해줘",
             author: "LICO / 성소작",
             point: 9.95,
-            thumbnail: "wed1.jpg"
+            thumbnail: "./wed1.jpg"
         },
         {
             title: "전지적 독자 시점",
             author: "UMI / 슬리피-C",
             point: 9.95,
-            thumbnail: "wed2.jpg"
+            thumbnail: "./wed2.jpg"
         },
         {
             title: "조조코믹스",
             author: "이동건",
             point: 9.97,
-            thumbnail: "wed3.jpg"
+            thumbnail: "./wed3.jpg"
         }
-    ]
+    ],
+
+    thu: [
+        {
+            title: "참교육",
+            author: "채용택/한가람",
+            point: 9.86,
+            thumbnail:"./mon1.jpg"
+        },
+        {
+            title: "뷰티풀 군바리",
+            author: "설이/윤성원",
+            point: 9.81,
+            thumbnail: "./mon2.jpg"
+        },
+        {
+            title: "퀘스트지상주의",
+            author: "박태준 만화회사",
+            point: 9.79,
+            thumbnail: "./mon3.jpg"
+        }
+    ],
+
+    fri: [
+        {
+            title: "김부장",
+            author: "박태준 만화회사/정종택",
+            point: 9.19,
+            thumbnail: "./tue1.jpg"
+        },
+        {
+            title: "여신강림",
+            author: "야옹이",
+            point: 9.38,
+            thumbnail: "./tue2.jpg"
+        },
+        {
+            title: "대학원 탈출일지",
+            author: "요다",
+            point: 9.98,
+            thumbnail: "./tue3.jpg"
+        }
+    ],
+
+    sat: [
+        {
+            title: "내 남편과 결혼해줘",
+            author: "LICO / 성소작",
+            point: 9.95,
+            thumbnail: "./wed1.jpg"
+        },
+        {
+            title: "전지적 독자 시점",
+            author: "UMI / 슬리피-C",
+            point: 9.95,
+            thumbnail: "./wed2.jpg"
+        },
+        {
+            title: "조조코믹스",
+            author: "이동건",
+            point: 9.97,
+            thumbnail: "./wed3.jpg"
+        }
+    ],
+
+    sun: [
+        {
+            title: "참교육",
+            author: "채용택/한가람",
+            point: 9.86,
+            thumbnail:"./mon1.jpg"
+        },
+        {
+            title: "뷰티풀 군바리",
+            author: "설이/윤성원",
+            point: 9.81,
+            thumbnail: "./mon2.jpg"
+        },
+        {
+            title: "퀘스트지상주의",
+            author: "박태준 만화회사",
+            point: 9.79,
+            thumbnail: "./mon3.jpg"
+        }
+    ],
 };
